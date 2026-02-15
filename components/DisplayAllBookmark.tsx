08281fd1abd3ef2754ea17e9d446f616f3c9b43e
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2026-02-15 20:11:47 +0530

    bug

--- components/DisplayAllBookmark.tsx
+++ components/DisplayAllBookmark.tsx
@@ -7,6 +7,7 @@ import { getUserByEmail } from "@/actions/user";
 
 const DisplayAllBookmark = () => {
     const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+    const [USER, setUSER] = useState<any>(null);
 
   type Bookmark = {
   id: string;
@@ -15,44 +16,64 @@ const DisplayAllBookmark = () => {
   createdAt: string;
    };
 
-  useEffect(() => {
-    fetchBookmarks();
-  }, []);
 
-  const fetchBookmarks = async () => {
-    const user =await getUserByEmail();
-    const { data } = await supabase.from("Bookmark").select("*").eq("userId", user?.id);
+
+
+   useEffect(() => {
+  let channel: any;
+
+  const init = async () => {
+    const user = await getUserByEmail();
+    if (!user) return;
+
+    setUSER(user);
+
+
+    const { data } = await supabase
+      .from("Bookmark")
+      .select("*")
+      .eq("userId", user.id);
+
     if (data) setBookmarks(data);
-  };
-  console.log(bookmarks);
 
-    useEffect(() => {
-    const channel = supabase
+    
+    channel = supabase
       .channel("bookmarks-channel")
       .on(
         "postgres_changes",
-        { event: "INSERT", schema: "public", table: "Bookmark" },
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "Bookmark",
+          filter: `userId=eq.${user.id}`,
+        },
         (payload) => {
           setBookmarks((prev) => [...prev, payload.new as Bookmark]);
         }
       )
-
       .on(
-      "postgres_changes",
-      { event: "DELETE", schema: "public", table: "Bookmark" },
-      (payload) => {
-        setBookmarks((prev) =>
-          prev.filter((b) => b.id !== payload.old.id)
-        );
-      }
-    )
-
+        "postgres_changes",
+        {
+          event: "DELETE",
+          schema: "public",
+          table: "Bookmark",
+          filter: `userId=eq.${user.id}`,
+        },
+        (payload) => {
+          setBookmarks((prev) =>
+            prev.filter((b) => b.id !== payload.old.id)
+          );
+        }
+      )
       .subscribe();
+  };
 
-    return () => {
-      supabase.removeChannel(channel);
-    };
-  }, []);
+  init();
+
+  return () => {
+    if (channel) supabase.removeChannel(channel);
+  };
+}, []);
 
   return (
     <div className="md:flex md:flex-wrap">
