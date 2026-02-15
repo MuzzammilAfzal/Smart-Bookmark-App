bc846acc94a81f7a327e7a8d47e00dfd53f6d85c
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2026-02-15 19:02:59 +0530

    production bug

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   
 
   return (
-   <div className="p-2 m-1">
+   <div className=" mb-10">
    {
     !session && <div className="w-full  flex  items-center justify-center mt-10 ">
      <span className="text-black">Please sign in to access the app.</span>
